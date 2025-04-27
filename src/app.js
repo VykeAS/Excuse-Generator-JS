ed06f291976d75c775cd0bb3304454d521fca0e2
Enrique <enrique2002.s.m@gmail.com>
2025-04-27 15:02:10 +0000

    arreglo de gramatica

--- src/app.js
+++ src/app.js
@@ -8,15 +8,10 @@ function generarExcusa() {
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-
   let rnd1 = Math.floor(Math.random() * who.length);
   let rnd2 = Math.floor(Math.random() * action.length);
   let rnd3 = Math.floor(Math.random() * what.length);
   let rnd4 = Math.floor(Math.random() * when.length);
-
-  // Genera la excusa combinando las partes
   let excuse = `${who[rnd1]} ${action[rnd2]} ${what[rnd3]} ${when[rnd4]}`;
-
-  // Muestra la excusa en el elemento con id "excuse"
   document.querySelector("#Excusa").innerHTML = excuse;
 }
